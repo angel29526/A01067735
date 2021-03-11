29396fdc09b7c65fdce41f8061d836466b158823
Ángel Flores Bermúdez <chiripitas2000@hotmail.com>
2021-03-11 01:49:38 -0600

    LAB 11 finalizado

--- Laboratorio 11/app.js
+++ Laboratorio 11/app.js
@@ -5,17 +5,20 @@ console.log("Ya no es necesario reiniciar el 'servidor', ya era hora.");
 //{nombre_atributo: valor}
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+const misRutas = require("./routes/clases");
+
 //Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+
 app.use((request, response, next) => {
     console.log('Middleware!');
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
-app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    response.send('¡Hola mundo!'); //Manda la respuesta
-});
-
+app.use("/", misRutas);
+            
 app.listen(3000);
---
+++ Laboratorio 11/routes/clases.js
@@ -0,0 +1,38 @@
+const clases = ["Saber", "Archer", "Lancer"];
+
+const { request, response } = require('express');
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/clases/nueva-clase",(request, response, next)=>{
+    response.send("<h1>Nueva clase</h1><body><h1>Agregar nueva clase</h1><form action='nueva-clase' method='POST'><input type='text' name='nombre'><input type='submit' value='Submit'></form>");
+});
+
+router.post("/clases/nueva-clase",(request, response, next)=>{
+    console.log(request.body.nombre);
+    clases.push(request.body.nombre);
+    response.send("<h1>Personaje Guardado, nice</h1>");
+    console.log(clases);
+});
+
+router.use('/clases',(request, response, next) => {
+    let htmlClases = "<h1>Clases</h1><h2><ul>";
+    for (let clase of clases){
+        htmlClases += "<li>" + clase + "</li>";
+    }
+    htmlClases += "</h2></ul>";
+    response.send(htmlClases); 
+});
+
+router.get('/',(request, response, next)=>{
+    response.send("<h1>Página principal, buenas</h1><p>Laboratorio 11, bastante interesante express, todo decente, ya era hora.</p>")
+});
+
+//404
+router.get('*', (request, response, next) => {
+    response.statusCode = 404;
+    response.send("<h1>Nada que ver por aquí, fuera.</h1>"); 
+});
+
+module.exports = router;
